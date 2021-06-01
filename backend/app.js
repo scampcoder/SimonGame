25ce8be2290c83480d5eb54cb34539b4f0e1b3e5
Victoria Kee <victoriakee95@gmail.com>
2021-06-01 15:01:17 -0500

    Grab initial variables

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let string = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector('#turn');
+const topLeft = document.querySelector('#topleft');
+const topRight = document.querySelector('#topright');
+const bottonLeft = document.querySelector('#bottomleft');
+const bottomRight = document.querySelector('#bottomright');
+const strict = document.querySelector('#strict');
+const onButton = document.querySelector('#on');
+const startButton = document.querySelector('#start');
+
