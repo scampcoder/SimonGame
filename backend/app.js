b77fc95fdc405d25a854cba67517eeb59723998d
Victoria Kee <victoriakee95@gmail.com>
2021-06-01 15:28:10 -0500

    nix dead bkgrnd img link

--- backend/app.js
+++ backend/app.js
@@ -15,17 +15,17 @@ const topLeft = document.querySelector('#topleft');
 const topRight = document.querySelector('#topright');
 const bottonLeft = document.querySelector('#bottomleft');
 const bottomRight = document.querySelector('#bottomright');
-const strict = document.querySelector('#strict');
+const strictButton = document.querySelector('#strict');
 const onButton = document.querySelector('#on');
 const startButton = document.querySelector('#start');
 
-startButton.addEventListener('click', (event) => {
+strictButton.addEventListener('click', (event) => {
     if (strictButton.checked == true){
         strict =  true;
     } else {
         strict = false;
     }
-})
+});
 
 onButton.addEventListener('click', (event) => {
     if (onButton.checked == true){
@@ -34,5 +34,28 @@ onButton.addEventListener('click', (event) => {
     } else {
         on = false;
         turnCounter.innerHTML = "";
+        clearColor();
+        clearInterval(intervalId);
     }
-})
+});
+
+startButton.addEventListener('click', (event) => {
+    if(on || win){
+        play();
+    }
+});
+
+function play() {
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    turnCounter.innerHTML = 1;
+    good = true;
+    for(let i=0; i < 20; i++){
+        order.push(Math.floor(Math.random() * 4) + 1);
+    }
+    console.log(order);
+}
